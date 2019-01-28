7f7ebbd3b53c84f38cc44eaf88dc63c907ef0abf
Ingo Bürk <ingo.buerk@tngtech.com>
2019-01-28 08:16:00 +0700

    Second Commit

--- app/Http/Controllers/BarangControllerAPI.php
+++ app/Http/Controllers/BarangControllerAPI.php
@@ -3,19 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Barang;
-use App\Http\Resources\Barang as BarangResource;
 
 class BarangControllerAPI extends Controller
 {
     //
-    public function BarangMasuk(Request $request){
-        $barang = $request->isMethod('put') ? Barang::findOrFail($request->barang_id) : new Barang;
-        $barang->id = $request->input('barang_id');
-        $barang->stok =  1;
+    public function BarangMasuk(Request $request, $id){
+        $stok =  $request->stok;
 
-        if($barang->save()) {
-            return new BarangResource($barang);
-        } 
+        $barang = Barang::find($id);
+        $barang->stok =  $barang->stok + $stok;
+        $barang->save();
+
+        $nama = $barang->nama;
+        return "Stok Barang $nama ditambah $stok";
+    }
+
+    public function BarangKeluar(Request $request, $id){
+        $stok =  $request->stok;
+
+        $barang = Barang::find($id);
+        $barang->stok =  $barang->stok - $stok;
+        $barang->save();
+
+        $nama = $barang->nama;
+        return "Stok Barang $nama dikurang $stok";
     }
 }
